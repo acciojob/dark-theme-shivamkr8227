0bd06897d23128ae68c8326f2e1534df237c0e82
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-27 22:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
-
+let button=document.getElementById("swap");
+let backgr=document.getElementById("app");
+let value=true;
+button.addEventListener("click", function(){
+	if(value==true)
+	{
+		backgr.style.backgroundColor="black";
+		value=false;
+	}
+	else
+	{
+		backgr.style.backgroundColor="white";
+		value=true;
+	}
+});
